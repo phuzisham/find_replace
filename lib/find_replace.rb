151ee455b2733729c1bbcd00f3a0d427a31485db
David Hale and Qudsia Ahmad <david@>
2017-09-06 11:41:27 -0700

    first spec passes

--- lib/find_replace.rb
+++ lib/find_replace.rb
@@ -1,10 +1,19 @@
 #!/usr/bin/env ruby
 
 class FRP
-  def initialize()
+  def initialize(sentence)
+    @str = sentence
   end
 
   def find_and_replace(target, change)
-    
+    string = @str.split(' ')
+    i = 0
+    while(i < string.length) do
+      if (string[i]  === target)
+        string[i] = change
+      end
+      i += 1
+    end
+    string.join(' ')
   end
 end
--- spec/find_replace_spec.rb
+++ spec/find_replace_spec.rb
@@ -2,6 +2,10 @@ require('rspec')
 require('pry')
 require('find_replace')
 
-describe('#') do
+ sample = FRP.new('Hello World')
 
+describe('#find_and_replace') do
+  it("finds and changes one word for another") do
+    expect(sample.find_and_replace("World", "Universe")).to(eq("Hello Universe"))
+  end
 end
